8fc9271f745b381ce5ae4a8d69f952f38d1b0192
cleidisonfeitoza <pequenocleidison21@gmail.com>
2024-05-23 15:28:03 -0300

    insert-api-produtos

---
+++ cypress/e2e/api/api_produtos.cy.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress"/>
+
+describe('API - produtos',()=>{
+    it ('Get usuarios',()=>{
+    cy.request({
+        method: 'GET',
+        url: 'http://localhost:3000/produtos',
+        
+    }).then((Response)=>{
+        expect(Response.status).to.equal(200)
+         
+    })
+
+    })
+})//fim
