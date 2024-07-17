04ead840e6bdc4d827f9d3a4b2bc68f2767f95ef
Cleidison Feitoza <118213344+CLEIDISON21@users.noreply.github.com>
2024-07-17 10:39:46 -0300

    Update login_testes.cy.js

--- cypress/e2e/Frontend/login_testes.cy.js
+++ cypress/e2e/Frontend/login_testes.cy.js
@@ -8,7 +8,7 @@ describe('teste funcionalidade login', () => {
 
   it('login com usuario invalido', () => {
     cy.login_teste('invalid_user','secret_sauce')
-    cy.get('[data-test="error"]').should('contain','Epic sadface: Username and password do not match any user in this services')
+    cy.get('[data-test="error"]').should('contain','Epic sadface: Username and password do not match any user in this service')
   })
 
   it('login com senha invalida', () => {
